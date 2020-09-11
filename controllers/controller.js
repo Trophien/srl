6061ba82229dfb9502f27ab1941fb12b71a4ea9e
leventenyiro <nyiro.levente@gmail.com>
2020-09-11 18:53:28 +0200

    add login and packages

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,41 @@
 var Database = require("../models/Database")
+var Bcrypt = require("../models/Bcrypt")
+
+function checkPassword(password) {
+    return password.match(/[a-z]+/) && password.match(/[A-Z]+/) && password.match(/[0-9]+/) ? true : false
+}
+
+exports.login = (req, res) => {
+    if (req.body.username == "" || req.body.password == "")
+        res.json({ error: "Something is missing!" })
+    else {
+        var db = new Database()
+        var bcrypt = new Bcrypt()
+        db.login(req, (result) => {
+            if (result.length == 0) {
+                res.json({ error: "Login is unsuccessful" })
+                db.end()
+            } else {
+                bcrypt.decrypt(req.body.password, result[0].password, (hash) => {
+                    if (hash) {
+                        req.session.userId = result[0].id
+                        res.json({ success: "" })
+                        db.end()
+                    } else {
+                        res.statusCode = 401
+                        res.json({ error: "Login is unsuccessful" })
+                        db.end()
+                    }
+                })
+            }
+        })
+    }
+}
+
+exports.logout = (req, res) => {
+    req.session.destroy((err) => {
+        if (err) throw err
+        res.clearCookie("session")
+        res.end()
+    })
+}
--- router.js
+++ router.js
@@ -2,4 +2,8 @@ var express = require("express")
 var router = express.Router()
 var controller = require("./controllers/controller")
 
+router.post("/login", controller.login)
+
+router.post("/logout", controller.logout)
+
 module.exports = router
